bddfd96c638158d79f5bc44496ecfa2a2be96b40
Pablo Alcantar <palcantar@lemontech.cl>
2015-06-24 12:24:40 -0300

    normaizing md5

--- runner.sh
+++ runner.sh
@@ -5,13 +5,14 @@ lastCheck=''
 while [ true ]; do
     ts=`date +"%s"`;
     ruby scrapper.rb > bd/$ts.json;
-    check=$(md5sum bd/$ts.json);
-
-    if [[ "$check" -eq "$lastCheck" ]]; then
+    check=$(openssl md5 bd/$ts.json | awk '{print $2}');
+    echo $check $lastCheck
+    if [ "$check" = "$lastCheck" ]; then
+        echo "equals"
         rm bd/$ts.json
     fi
 
     lastCheck=$(md5sum bd/$ts.json);
 
-    sleep 60;
+    sleep 3;
 done
